09eb29b087f4ef0da4986b2e57d54ed6f940618e
Maqiora <eosd1142@gmail.com>
2025-06-13 01:05:48 +0200

    Another merge conflict

--- rozliczki/urls.py
+++ rozliczki/urls.py
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.contrib import admin
+from ksiegowosc import views  # only needed if you use it somewhere
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/accounts/', permanent=False)),  # redirect homepage to accounts
+    path('', RedirectView.as_view(url='/accounts/', permanent=False)),
     path('accounts/', include('ksiegowosc.urls')),
     path('admin/', admin.site.urls),
 ]
-=======
-from django.contrib import admin
-from django.urls import path, include
-from ksiegowosc import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("ksiegowosc.urls"))
-]
-
-
->>>>>>> 8702e905c9eb88b49bca0485c8db1f00fd1d7221
