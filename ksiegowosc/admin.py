540b5822d46ab984558060330bac453c3281b097
Maqiora <eosd1142@gmail.com>
2025-06-13 01:07:51 +0200

    Yet another merge conflict

--- ksiegowosc/admin.py
+++ ksiegowosc/admin.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Account
+from .models import Account, wydatki
 
-# Register your models here.
-
-admin.register(Account)
+# Register Account with a custom ModelAdmin
+@admin.register(Account)
 class AccountMod(admin.ModelAdmin):
     list_display = ('name', 'account_type', 'created_at', 'owner', 'deleted_at')
     list_filter = ('account_type', 'deleted_at')
-   
-=======
-from .models import wydatki
 
-# Register your models here.
-
+# Register wydatki normally
 admin.site.register(wydatki)
->>>>>>> 8702e905c9eb88b49bca0485c8db1f00fd1d7221
+
