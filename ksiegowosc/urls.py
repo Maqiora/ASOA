226fe64e462cf31e3aa0d07accf4216fd4f4e230
Maqiora <eosd1142@gmail.com>
2025-06-13 01:03:20 +0200

    resolved some merge stuff in urls.py

--- ksiegowosc/urls.py
+++ ksiegowosc/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path 
-<<<<<<< HEAD
+from django.urls import path
+from . import views
 from .views import (
     AccountListView,
     AccountCreateView,
@@ -8,16 +8,10 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', AccountListView.as_view(), name='account_list'),  
+    path("", views.home, name="home"),
+    path("wydatki/", views.wydatki, name="wydatki"),
+    path('', AccountListView.as_view(), name='account_list'),
     path('create/', AccountCreateView.as_view(), name='account_create'),
     path('<int:pk>/edit/', AccountUpdateView.as_view(), name='account_edit'),
     path('<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
 ]
-=======
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("wydatki/", views.wydatki, name="wydatki")
-]
->>>>>>> 8702e905c9eb88b49bca0485c8db1f00fd1d7221
