9dee42ef921ee1fbd568e381473a54d27cf04e82
Maqiora <eosd1142@gmail.com>
2025-06-13 15:22:26 +0200

    adding new list view

--- ksiegowosc/urls.py
+++ ksiegowosc/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import (
-    AccountListView,
     AccountCreateView,
     AccountUpdateView,
     AccountDeleteView,
@@ -9,7 +8,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('accounts/', Account_list_and_create, name='account_add'),
+    path('accounts/', Account_list_and_create.as_view, name='account_add'),
     path('create/', AccountCreateView.as_view(), name='account_create'),
     path('<int:pk>/edit/', AccountUpdateView.as_view(), name='account_edit'),
     path('<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
--- ksiegowosc/views.py
+++ ksiegowosc/views.py
@@ -1,20 +1,32 @@
+from django.views import View
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic.list import MultipleObjectMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Account
 from .forms import AccountForm
 
-def Account_list_and_create(request):
-    if request.method == 'POST':
-        form = AccountForm(request.POST)
+class AccountListCreateView(View, MultipleObjectMixin):
+    model = Account
+    form_class = AccountForm
+    template_name = 'ksiegowosc/account_list.html'
+    context_object_name = 'accounts'
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        form = self.form_class()
+        context = self.get_context_data(form=form)
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
             return redirect('account_list')
-        
-def home(request):
-    return render(request, 'home.html')
+        context = self.get_context_data(form=form)
+        return render(request, self.template_name, context)
  
 class AccountCreateView(CreateView):
     model = Account
