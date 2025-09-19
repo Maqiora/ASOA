c1e8da9de16bdef006b0f4869a59b003081e5111
Maqiora <eosd1142@gmail.com>
2025-09-19 07:04:25 +0200

    work in progress

--- ksiegowosc/forms.py
+++ ksiegowosc/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Account
+from .models import Account, Transaction
 
 class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['name', 'account_type', 'owner']
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['date', 'description', 'debit_account', 'credit_account','net_amount', 'tax_amount', 'currency']
--- ksiegowosc/views.py
+++ ksiegowosc/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView, CreateView, UpdateView, DeleteVie
 from django.urls import reverse_lazy
 
 from .models import Account, Transaction
-from .forms import AccountForm
+from .forms import AccountForm, TransactionForm
 
 
 class DashboardView(TemplateView):
@@ -13,18 +13,28 @@ class DashboardView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["accounts"] = Account.active.all()
         context["transactions"] = Transaction.active.all()
-        context["form"] = AccountForm()
+        context["account_form"] = AccountForm()
+        context["transaction_form"] = TransactionForm()
         return context
 
     def post(self, request, *args, **kwargs):
-        """Handle Account form submission directly on the dashboard."""
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("dashboard")
-        context = self.get_context_data()
-        context["form"] = form
-        return self.render_to_response(context)
+        if "account_submit" in request.POST:
+            form = AccountForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect("dashboard")
+            context = self.get_context_data()
+            context["account_form"] = form
+            return self.render_to_response(context)
+        
+        elif "transaction_submit" in request.POST:
+            form = TransactionForm(request.POST)
+            if form.is_valid():
+                form.save(user=request.user)
+                return redirect("dashboard")
+            context = self.get_context_data()
+            context["transaction_form"] = form
+            return self.render_to_response(context)
 
 
 class AccountUpdateView(UpdateView):
@@ -33,10 +43,26 @@ class AccountUpdateView(UpdateView):
     template_name = "ksiegowosc/account_form.html"
     success_url = reverse_lazy("dashboard")
 
+    def get_queryset(self):
+        return Account.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = AccountForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(self.success_url)
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
+
 
 class AccountDeleteView(DeleteView):
     model = Account
-    template_name = "ksiegowosc/account_confirm_delete.html"
+    template_name = "ksiegowosc/account_delete.html"
     success_url = reverse_lazy("dashboard")
 
     def post(self, request, *args, **kwargs):
